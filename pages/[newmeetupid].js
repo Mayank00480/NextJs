b231ac99da68428d0c93fb0e785d8fa414b4fdae
Mayank00480 <mayanksharma1937@gmail.com>
2023-11-20 12:59:08 +0530

    Details page work properly

--- pages/[newmeetupid].js
+++ pages/[newmeetupid].js
@@ -1,35 +1,29 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 import MeetUpDetails from '@/components/meetups/MeetUpDetails'
-
+import { MongoClient } from 'mongodb'
 const MeetUpItems = (props) => {
     const router = useRouter()
   return (
     <MeetUpDetails id = {props.id}/>
   )
 }
-export function getStaticPaths(){
+export async function getStaticPaths(){
+  const client = await MongoClient.connect('mongodb+srv://mayanksharma1937:1XG5ZTABdDArvz9D@cluster0.rx6kopz.mongodb.net/');
+  const db = client.db();
+  const collection = db.collection('meetup')
+  const meetups = await collection.find({} , {_id : 1}).toArray();
 return {
 fallback : false,
-  paths : [
-    {
+  paths : meetups.map(item => {
+    return {
       params : {
-        newmeetupid : 'm1'
-      }
-    },
-    {
-      params : {
-        newmeetupid : 'm2'
-      }
-    },
-    {
-      params : {
-        newmeetupid : 'm3'
+        newmeetupid : item._id.toString()
       }
     }
-  ]
-}
-}
+  })
+
+}}
 export async function getStaticProps(context) {
 const id = context.params.newmeetupid;
   return {
